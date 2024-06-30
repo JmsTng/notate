784879ec223cfca0bf256ef1c6e67055594ff847
James Tung <james.ftung@gmail.com>
2024-06-29 23:46:58 -0400

    split definition of components into a function

--- notate.py
+++ notate.py
@@ -13,18 +13,26 @@ def run():  # entrypoint from main.py
     return notate(window)
     
     
-def notate(window):
-    # renderer
+def load_comps(window):
     renderer = sdl2.ext.renderer.Renderer(window, logical_size=(640, 480))
     renderer.color = (0, 0, 0)
 
     sprite_factory = sdl2.ext.SpriteFactory(renderer=renderer)
+    
     font = sdl2.ext.ttf.FontTTF(
         font=os.path.join(os.path.curdir, "res", "Red_Hat_Display", "static", "RedHatDisplay-Regular.ttf"),
         size=120,
         color=(255, 255, 255)
     )
+
     text_input = Input()
+
+    return renderer, sprite_factory, font, text_input
+
+
+def notate(window):
+    renderer, sprite_factory, font, text_input = load_comps(window)
+
     running = True
     
     # event loop
